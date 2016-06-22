dfbd042f63e8b9a796dbb383fea789091d92bcbe
Pivopil <myworkplace8@gmail.com>
2016-06-22 17:07:13 +0300

    Role repo has been added

---
+++ share/src/main/java/io/github/pivopil/share/persistence/RoleRepository.java
@@ -0,0 +1,12 @@
+package io.github.pivopil.share.persistence;
+
+import io.github.pivopil.share.entities.impl.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created on 06.10.15.
+ */
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
