0fcc63ceb1d4452b8b57bbd032778ac5f8a76213
Pivopil <myworkplace8@gmail.com>
2016-11-14 15:12:11 +0200

    spring session bug fix

--- rest/src/main/java/io/github/pivopil/rest/config/WebSocketMessageBrokerConfig.java
+++ rest/src/main/java/io/github/pivopil/rest/config/WebSocketMessageBrokerConfig.java
@@ -1,12 +1,16 @@
 package io.github.pivopil.rest.config;
 
+import io.github.pivopil.rest.handlers.CustomHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.ExpiringSession;
+import org.springframework.session.SessionRepository;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 /**
  * Created on 02.11.16.
@@ -17,7 +21,10 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketMessageBrokerConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
 
     protected void configureStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/messages").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/messages")
+                .addInterceptors(new CustomHandshakeInterceptor())
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     public void configureMessageBroker(MessageBrokerRegistry registry) {
---
+++ rest/src/main/java/io/github/pivopil/rest/handlers/CustomHandshakeInterceptor.java
@@ -0,0 +1,31 @@
+package io.github.pivopil.rest.handlers;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+import static org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor.HTTP_SESSION_ID_ATTR_NAME;
+
+/**
+ * Created on 14.11.16.
+ */
+public class CustomHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request.getURI().getQuery().split("access_token=").length == 2) {
+            String token = request.getURI().getQuery().split("access_token=")[0];
+            attributes.put("SPRING.SESSION.ID", token);
+            attributes.put(HTTP_SESSION_ID_ATTR_NAME, token);
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
